e92350cc0a244a4596998fd5177176b1758d964e
Marcus Kida <marcus@kida.io>
2016-02-04 16:28:21 +1100

    Use `curl_easy_setopt_string` and `curl_easy_setopt_bool`

--- Source/main.swift
+++ Source/main.swift
@@ -12,8 +12,8 @@ serve { _ in
   let url = "https://github.com"
 
   let handle = curl_easy_init()
-  curl_easy_setopt(handle, CURLOPT_URL, url)
-  curl_easy_setopt(handle, CURLOPT_VERBOSE, true)
+  curl_easy_setopt_string(handle, CURLOPT_URL, url)
+  curl_easy_setopt_bool(handle, CURLOPT_VERBOSE, true)
 
   let ret = curl_easy_perform(handle)
   let error = curl_easy_strerror(ret)
