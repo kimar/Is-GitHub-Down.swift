62a64f368eda56779a158440c348944d633580fe
Marcus Kida <marcus@kida.io>
2016-02-04 16:20:54 +1100

    Use CcURL

--- Package.swift
+++ Package.swift
@@ -4,8 +4,6 @@ let package = Package(
   name: "Is-GitHub-Down",
   dependencies: [
     .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/tidwall/Safe.git", majorVersion: 1, minor: 2),
-    .Package(url: "https://github.com/interstateone/Zeal.git", majorVersion: 0, minor: 1),
-    .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
+    .Package(url: "https://github.com/PureSwift/CcURL.git", majorVersion: 1, minor: 1)
   ]
 )
--- Source/main.swift
+++ Source/main.swift
@@ -3,23 +3,19 @@ import Glibc
 #endif
 import Inquiline
 import Curassow
-import Safe
-import Zeal
+import CcURL
 
 serve { _ in
-  let result = Chan<HTTP.Response>()
+  let url: CString = "https://github.com"
 
-  HTTPClient(host: "github.com", port: 443)
-  .get("/") { result in
-    do {
-      let response = try result()
-      result <-response
-    } catch {
-      // something bad happened :(
-      // I mean really, really bad 🚀
-    }
-  }
+  curl_easy_setopt(handle, CURLOPT_URL, url)
+  curl_easy_setopt(handle, CURLOPT_VERBOSE, true)
 
-  let response = <-result
-  return Response(.Ok, contentType: "text/plain", body: response.string)
+  let ret = curl_easy_perform(handle)
+  let error = curl_easy_strerror(ret)
+
+  print("ret = \(ret)")
+  print("error = \(error)")
+
+  return Response(.Ok, contentType: "text/plain", body: ret)
 }
